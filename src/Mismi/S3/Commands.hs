e26d2dadcda679c7c62aaee81fbde7398bc965d9
markhibberd <mark@hibberd.id.au>
2015-03-26 18:01:35 +1100

    Add download command.

--- src/Mismi/S3/Commands.hs
+++ src/Mismi/S3/Commands.hs
@@ -5,6 +5,7 @@ module Mismi.S3.Commands (
     exists
   , delete
   , read
+  , download
   , write
   , getObjects
   , listRecursively
@@ -12,11 +13,13 @@ module Mismi.S3.Commands (
 
 import qualified Aws.S3 as S3
 
+import           Control.Monad.IO.Class
 import           Control.Monad.Trans.Class (lift)
 import           Control.Monad.Catch (catch, throwM)
 
 import           Data.ByteString as BS
 import           Data.Conduit
+import           Data.Conduit.Binary (sinkFile)
 import           Data.Conduit.List as C
 import qualified Data.List.NonEmpty as NEL
 import           Data.Text as T
@@ -33,6 +36,10 @@ import           P
 
 import           Prelude (error)
 
+import           System.FilePath
+import           System.Directory
+
+
 exists :: Address -> S3Action Bool
 exists a =
   let req = S3.headObject (unBucket $ bucket a) (unKey $ key a) in
@@ -48,6 +55,12 @@ read a =
   (awsRequest get >>=
    fmap Just . lift . fmap (T.decodeUtf8 . BS.concat) . ($$+- C.consume) . responseBody . S3.gorResponse)
   `catch` (\(e :: S3.S3Error) -> if S3.s3StatusCode e == status404 then pure Nothing else throwM e)
+
+download :: Address -> FilePath -> S3Action ()
+download a p =
+  let get = S3.getObject (unBucket $ bucket a) (unKey $ key a) in do
+    liftIO $ createDirectoryIfMissing True (dropFileName p)
+    (awsRequest get >>= lift . ($$+- (sinkFile p)) . responseBody . S3.gorResponse)
 
 write :: Address -> Text -> S3Action ()
 write a t =
