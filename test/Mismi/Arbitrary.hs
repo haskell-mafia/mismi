be80122b154e175526b1568cefef71c185c85ea6
Dom De Re <domdere@domdere.com>
2015-04-21 16:53:04 +1000

    Adding uuids to test addresses and tmp keys

--- test/Mismi/Arbitrary.hs
+++ test/Mismi/Arbitrary.hs
@@ -20,5 +20,5 @@ instance Arbitrary Key where
   -- https://github.com/ambiata/vee/issues/7
   arbitrary =
     let genPath = elements ["happy", "sad", ".", ":", "-"]
-        path = T.dropWhileEnd ('/' ==) . T.take 256 . T.intercalate "/" <$> listOf1 (T.intercalate "" <$> listOf1 genPath)
+        path = T.dropWhileEnd ('/' ==) . T.take 128 . T.intercalate "/" <$> listOf1 (T.intercalate "" <$> listOf1 genPath)
     in (Key . append "tests/") <$> path
--- test/Mismi/Test/S3.hs
+++ test/Mismi/Test/S3.hs
@@ -10,6 +10,8 @@ import           Control.Monad.Catch (bracket_, finally)
 import qualified Data.List as L
 import           Data.Text as T
 import           Data.Text.Encoding as T
+import           Data.UUID ( toString )
+import           Data.UUID.V4 ( nextRandom )
 
 import           Network.HTTP.Client (RequestBody(..))
 
@@ -25,15 +27,34 @@ import           Mismi.Test
 import           Orphanarium.Corpus
 
 
+newtype Unique a = Unique a deriving (Show, Eq)
+
+runUnique :: Unique a -> a
+runUnique (Unique x) = x
+
+instance Functor Unique where
+    fmap f (Unique x) = Unique $ f x
+
+makeUniqueKey :: Key -> IO (Unique Key)
+makeUniqueKey k = do
+    u <- nextRandom
+    let ut = T.pack $ toString u
+    return . Unique $ Key "tests" </> Key ut </> k
+
 data KeyTmp = KeyTmp {
-    tmpPath :: Key
+    tmpPath' :: Key
   , tmpBody :: Text
   } deriving (Eq, Show)
 
+tmpPath :: KeyTmp -> Key
+tmpPath = (Key "tmp/vee" </>) . tmpPath'
+
+makeUniqueKeyTmp :: KeyTmp -> IO (Unique KeyTmp)
+makeUniqueKeyTmp (KeyTmp k b) = fmap (flip KeyTmp b) <$> makeUniqueKey k
 
 -- Ensure everything is under our own key space for debugging
 instance Arbitrary KeyTmp where
-  arbitrary = KeyTmp  <$> ((Key "tmp/vee" </>) <$> arbitrary) <*> arbitrary
+  arbitrary = KeyTmp  <$> arbitrary <*> arbitrary
 
 data LocalPath =
   LocalPath {
@@ -53,18 +74,24 @@ testBucket =
 (<//>) :: KeyTmp -> Key -> KeyTmp
 (<//>) (KeyTmp k1 b) k2 = KeyTmp (k1 </> k2) b
 
-withTmpKey :: KeyTmp -> S3Action t -> S3Action t
-withTmpKey (KeyTmp (Key tmpPath') body') f = do
+withTmpKey :: KeyTmp -> (Unique Address -> S3Action t) -> S3Action t
+withTmpKey kt f = do
   (Bucket bucket') <- liftIO testBucket
+  u <- liftIO $ makeUniqueKeyTmp kt
+  let uniqueTmpPath = tmpPath . runUnique $ u
+  let body' = tmpBody . runUnique $ u
   bracket_
-    (awsRequest (S3.putObject bucket' tmpPath' (RequestBodyBS (T.encodeUtf8 body'))))
-    (awsRequest $ S3.DeleteObject tmpPath' bucket')
-    f
+    (awsRequest (S3.putObject bucket' (unKey uniqueTmpPath) (RequestBodyBS (T.encodeUtf8 body'))))
+    (awsRequest $ S3.DeleteObject (unKey uniqueTmpPath) bucket')
+    (f . Unique $ Address (Bucket bucket') uniqueTmpPath)
 
-withKey :: Key -> S3Action t -> S3Action t
+withKey :: Key -> (Unique Address -> S3Action t) -> S3Action t
 withKey k f = do
-  (Bucket bucket') <- liftIO testBucket
-  finally f (awsRequest $ S3.DeleteObject (unKey k) bucket')
+    bucket' <- liftIO testBucket
+    withAddress (Address bucket' k) f
 
-withAddress :: Address -> S3Action t -> S3Action t
-withAddress a f = finally f (awsRequest $ S3.DeleteObject (unKey $ key a) (unBucket $ bucket a))
+withAddress :: Address -> (Unique Address -> S3Action t) -> S3Action t
+withAddress a f = do
+    u <- liftIO . makeUniqueKey $ key a
+    let uniqueAddress = fmap (Address (bucket a)) u
+    finally (f uniqueAddress) (awsRequest $ S3.DeleteObject (unKey . runUnique $ u) (unBucket . bucket $ a))
