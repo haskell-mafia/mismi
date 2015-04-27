6e6df1d9304f865713ba51b3a5bb1945ad304bd9
Tim McGilchrist <tim.mcgilchrist@ambiata.com>
2015-04-27 15:47:57 +1000

    Add queuename data type.

---
+++ src/Mismi/SQS/Data.hs
@@ -0,0 +1,17 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+module Mismi.SQS.Data (
+    QueueName(..)
+  ) where
+
+import           Data.Text
+import           P
+
+-- Queue names are limited to 80 characters. Alphanumeric characters
+-- plus hyphens (-) and underscores (_) are allowed. Queue names must be unique
+-- within an AWS account. After you delete a queue, you can reuse the queue
+-- name.
+--
+newtype QueueName = QueueName {
+    unQueueName :: Text
+  } deriving (Eq, Show)
