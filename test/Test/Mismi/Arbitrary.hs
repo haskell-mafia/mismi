3f73d6576e7a807f994ada226a9272946b4600bc
Charles O'Farrell <charleso@charleso.org>
2015-04-28 12:56:24 +1000

    Add QueueName Arbitrary

--- test/Test/Mismi/Arbitrary.hs
+++ test/Test/Mismi/Arbitrary.hs
@@ -6,6 +6,7 @@ module Test.Mismi.Arbitrary where
 import           Data.Text as T
 
 import           Mismi.S3.Data
+import           Mismi.SQS.Data
 
 import           Test.Mismi
 
@@ -23,3 +24,11 @@ instance Arbitrary Key where
     let genPath = elements ["happy", "sad", ".", ":", "-"]
         path = T.dropWhileEnd ('/' ==) . T.take 256 . T.intercalate "/" <$> listOf1 (T.intercalate "" <$> listOf1 genPath)
     in (Key . append "tests/") <$> path
+
+instance Arbitrary QueueName where
+  arbitrary = (QueueName . T.pack) <$> sized (\n -> vectorOf (max 80 n) (oneof [
+      choose ('a', 'z')
+    , choose ('0', '9')
+    , pure '-'
+    , pure '_'
+    ]))
