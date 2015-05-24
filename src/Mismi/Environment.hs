8783b44ce23030627d7c1e69ad122917816b293b
Charles O'Farrell <charleso@charleso.org>
2015-05-25 09:34:42 +1000

    Extract getRegionFromEnv from SQS to separate module

---
+++ src/Mismi/Environment.hs
@@ -0,0 +1,42 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+module Mismi.Environment (
+    Region (..)
+  , RegionError (..)
+  , getRegionFromEnv
+  , regionErrorRender
+  ) where
+
+
+import           Control.Monad.Reader
+import           Control.Monad.Trans.AWS
+
+import           Data.Bifunctor
+import           Data.Text as T
+
+import           Network.AWS.Data
+
+import           P
+
+import           System.Environment
+import           System.IO
+
+
+data RegionError =
+    RegionMissing
+  | RegionUnknown Text
+  deriving (Eq, Show)
+
+
+getRegionFromEnv :: IO (Either RegionError Region)
+getRegionFromEnv =
+  lookupEnv "AWS_DEFAULT_REGION" >>= \r ->
+    pure .
+      (=<<) (\r' -> first (const . RegionUnknown $ T.pack r') . fromText . T.pack $ r')
+      . maybeToRight RegionMissing
+      $ r
+
+
+regionErrorRender :: RegionError -> Text
+regionErrorRender RegionMissing = "Could not find the required $AWS_DEFAULT_REGION environment variable"
+regionErrorRender (RegionUnknown r) = "Unknown region: " <> r
--- src/Mismi/SQS/Control.hs
+++ src/Mismi/SQS/Control.hs
@@ -3,10 +3,7 @@
 {-# LANGUAGE RankNTypes #-}
 module Mismi.SQS.Control (
     SQSAction
-  , Network.AWS.Types.Region(..)
-  , RegionError(..)
   , runSQSWithCfg
-  , getRegionFromEnv
   , regionTo
   ) where
 
@@ -20,28 +17,19 @@ import qualified Network.AWS.Types
 import           Control.Monad.Reader hiding (forM)
 import           Control.Monad.Trans.Resource (ResourceT)
 
-import           Data.Bifunctor
-import           Data.List (lookup)
 import           Data.Maybe
-import           Data.Text
 
 import           Mismi.SQS.Data
-
-import           Network.AWS.Types
-import           Network.AWS.Data
 
 import           Network.HTTP.Client (Manager)
 import           Network.HTTP.Conduit (withManager)
 
 import           P
 
-import           System.Environment
 import           System.IO
 
 -- | Specilised AwsAction for SQS operations
 type SQSAction = ReaderT (Aws.Configuration, SQS.SqsConfiguration Aws.NormalQuery, Manager) (ResourceT IO)
-
-newtype RegionError = RegionError { unRegionError :: Text } deriving (Eq, Show)
 
 runSQSWithCfg :: Aws.Configuration -> Aws.Sqs.Core.Endpoint -> SQSAction a -> IO a
 runSQSWithCfg cfg endpoint' action =
@@ -60,9 +48,3 @@ regionTo r = case r of
          Network.AWS.Types.Frankfurt         -> Just sqsEndpointEuCentral1
          Network.AWS.Types.SaoPaulo          -> Just sqsEndpointSaEast1
          _                                   -> Nothing
-
-getRegionFromEnv :: IO (Either RegionError Region)
-getRegionFromEnv = liftIO $ do
-  env <- getEnvironment
-  let r = fromMaybe "" $ fmap pack $ lookup "AWS_DEFAULT_REGION" env
-  pure . first (RegionError . pack) . fromText $ r
