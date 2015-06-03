ef1208ecad28183aa4b14ae341cdbaff0d0f4c1a
mth <mark@hibberd.id.au>
2015-06-03 10:43:11 +1000

    Clean-up tests.

--- test/Test/IO/Mismi/S3/Commands.hs
+++ test/Test/IO/Mismi/S3/Commands.hs
@@ -34,144 +34,99 @@ import           System.IO.Temp
 
 import           Test.Mismi
 import           Test.Mismi.S3
-import           Test.QuickCheck.Monadic
 
-prop_exists :: Token -> Property
-prop_exists t = monadicIO $ do
-  r <- run $ do
-    runS3WithDefaults . withToken t $ \a -> do
-      write a ""
-      exists a
-  stop $ r === True
 
-prop_exists_empty :: Token -> Property
-prop_exists_empty t = monadicIO $ do
-  r <- run $
-    runS3WithDefaults . withToken t $ \a ->
-      exists a
-  stop $ r === False
+prop_exists :: Property
+prop_exists = testS3 $ \a -> do
+  write a ""
+  exists a
 
-prop_delete :: WriteMode -> Token -> Property
-prop_delete w t = monadicIO $ do
-  r <- run $
-    runS3WithDefaults . withToken t $ \a -> do
-      writeWithMode w a ""
-      x <- exists a
-      delete a
-      y <- exists a
-      pure (x, y)
-  stop $ r == (True, False)
+prop_exists_empty :: Property
+prop_exists_empty = testS3 $ \a ->
+  not <$> exists a
 
-prop_delete_empty :: Token -> Property
-prop_delete_empty t = monadicIO $ do
-    r <- run $ (
-      runS3WithDefaults . withToken t $ \a -> do
-        delete a
-        pure $ True)
-         `catchAll`
-         (\(_) -> pure $ False)
-    stop $ r === True
+prop_delete :: WriteMode -> Property
+prop_delete w = testS3 $ \a -> do
+  writeWithMode w a ""
+  x <- exists a
+  delete a
+  y <- exists a
+  pure $ (x, y) === (True, False)
 
-prop_read_write :: Token -> Text -> Property
-prop_read_write t d = monadicIO $ do
-    r <- run $
-      runS3WithDefaults . withToken t $ \a -> do
-        write a d
-        read a
-    stop $ r === Just d
+prop_delete_empty :: Property
+prop_delete_empty = testS3 $ \a ->
+  (True <$ delete a) `catchAll` (const . pure $ False)
 
-prop_write_download :: Token -> Text -> LocalPath -> Property
-prop_write_download tt d l = monadicIO $ do
-    r <- run $
-      withSystemTempDirectory "mismi" $ \p ->
-        runS3WithDefaults . withToken tt $ \a -> do
-          write a d
-          let t = p F.</> localPath  l
-          download a t
-          liftIO $ T.readFile t
-    stop $ r === d
+prop_read_write :: Text -> Property
+prop_read_write d = testS3 $ \a -> do
+  write a d
+  r <- read a
+  pure $ r === Just d
 
-prop_write_download_overwrite :: Token -> Text -> Text -> LocalPath -> Property
-prop_write_download_overwrite tt d e l = monadicIO $ do
-    r <- run $
-      withSystemTempDirectory "mismi" $ \p ->
-        runS3WithDefaults . withToken tt $ \a -> do
-          write a d
-          let t = p F.</> localPath  l
-          downloadWithMode Fail a t
-          write a e
-          downloadWithMode Overwrite a t
-          liftIO $ T.readFile t
-    stop $ r === e
+prop_write_download :: Text -> LocalPath -> Property
+prop_write_download d l = testLocalS3 $ \p a -> do
+  write a d
+  let t = p F.</> localPath  l
+  download a t
+  res <- liftIO $ T.readFile t
+  pure $ res === d
 
-prop_write_download_fail :: Token -> Text -> Text -> LocalPath -> Property
-prop_write_download_fail tt d e l = monadicIO $ do
-    r <- run $
-      withSystemTempDirectory "mismi" $ \p ->
-        runS3WithDefaults . withToken tt $ \a -> (do
-          write a d
-          let t = p F.</> localPath  l
-          downloadWithMode Fail a t
-          write a e
-          downloadWithMode Fail a t
-          pure False) `catchAll` (const . pure $ True)
-    stop $ r
+prop_write_download_overwrite :: Text -> Text -> LocalPath -> Property
+prop_write_download_overwrite old new l = testLocalS3 $ \p a -> do
+  write a old
+  let t = p F.</> localPath  l
+  downloadWithMode Fail a t
+  write a new
+  downloadWithMode Overwrite a t
+  r <- liftIO $ T.readFile t
+  pure $ r === new
 
-prop_upload :: Token -> Text -> LocalPath -> Property
-prop_upload tt d l = monadicIO $ do
-    r <- run $ do
-      withSystemTempDirectory "mismi" $ \p -> do
-        runS3WithDefaults . withToken tt $ \a -> do
-          let t = p F.</> localPath l
-          liftIO . D.createDirectoryIfMissing True $ F.takeDirectory t
-          liftIO $ T.writeFile t d
-          upload t a
-          read a
-    stop $ r === Just d
+prop_write_download_fail :: Text -> Text -> LocalPath -> Property
+prop_write_download_fail old new l = testLocalS3 $ \p a -> do
+  write a old
+  let t = p F.</> localPath  l
+  downloadWithMode Fail a t
+  write a new
+  (False <$ downloadWithMode Fail a t) `catchAll` (const . pure $ True)
 
-prop_upload_multipart :: Token -> LocalPath -> Property
-prop_upload_multipart tt l = forAll arbitrary $ \bs -> monadicIO $ do
-  r <- run $ do
-    withSystemTempDirectory "mismi" $ \p -> do
-      runS3WithDefaults . withToken tt $ \a -> do
-        let t = p F.</> localPath l
-        liftIO . D.createDirectoryIfMissing True $ F.takeDirectory t
-        liftIO $ withFile t WriteMode $ \h ->
-          replicateM_ 1000 (LBS.hPut h (LBS.fromChunks . return $ (BS.concat . L.replicate 10000 $ bs)))
-        upload t a
-        exists a
-  assert $ r == True
+prop_upload :: Text -> LocalPath -> Property
+prop_upload d l = testLocalS3 $ \p a -> do
+  let t = p F.</> localPath l
+  liftIO . D.createDirectoryIfMissing True $ F.takeDirectory t
+  liftIO $ T.writeFile t d
+  upload t a
+  r <- read a
+  pure $ r === Just d
 
-prop_write_failure :: Token -> Text -> Property
-prop_write_failure t d = monadicIO $ do
-    r <- run $ (
-      runS3WithDefaults . withToken t $ \a -> do
-        write a d
-        write a d
-        pure False)
-         `catchAll`
-         (\(_) -> pure $ True)
-    stop $ r === True
+prop_upload_multipart :: LocalPath -> Property
+prop_upload_multipart l = forAll arbitrary $ \bs -> testLocalS3 $ \p a -> do
+  let t = p F.</> localPath l
+  liftIO . D.createDirectoryIfMissing True $ F.takeDirectory t
+  liftIO $ withFile t WriteMode $ \h ->
+    replicateM_ 1000 (LBS.hPut h (LBS.fromChunks . return $ (BS.concat . L.replicate 10000 $ bs)))
+  upload t a
+  exists a
 
-prop_write_overwrite :: Token -> UniquePair Text -> Property
-prop_write_overwrite t (UniquePair x y) = monadicIO $ do
-    r <- run $
-        runS3WithDefaults . withToken t $ \a -> do
-            writeWithMode Fail a x
-            writeWithMode Overwrite a y
-            read a
-    stop $ r === pure y
+prop_write_failure :: Text -> Property
+prop_write_failure d = testS3 $ \a -> do
+  write a d
+  (False <$ write a d) `catchAll` (const . pure $ True)
+
+prop_write_overwrite :: UniquePair Text -> Property
+prop_write_overwrite (UniquePair x y) = testS3 $ \a -> do
+  writeWithMode Fail a x
+  writeWithMode Overwrite a y
+  r <- read a
+  pure $ r === pure y
 
 -- |
 -- If the object does not exist, then the behaviour should be invariant with the WriteMode
 --
-prop_write_nonexisting :: WriteMode -> Token -> Text -> Property
-prop_write_nonexisting w tt t = monadicIO $ do
-    r <- run $
-        runS3WithDefaults . withToken tt $ \a -> do
-            writeWithMode w a t
-            read a
-    stop $ r === pure t
+prop_write_nonexisting :: WriteMode -> Text -> Property
+prop_write_nonexisting w t = testS3 $ \a -> do
+  writeWithMode w a t
+  r <- read a
+  pure $ r === pure t
 
 prop_read_empty :: Key -> Property
 prop_read_empty k = ioProperty $ do
@@ -179,61 +134,66 @@ prop_read_empty k = ioProperty $ do
   t <- runS3WithDefaults . read $ Address bucket' k
   pure $ t === Nothing
 
-prop_getObjects_empty :: Token -> Property
-prop_getObjects_empty t = ioProperty . runS3WithDefaults . withToken t $ \a -> do
+prop_getObjects_empty :: Property
+prop_getObjects_empty = testS3 $ \a -> do
   objs <- getObjectsRecursively $ a
   pure $ fmap S3.objectKey objs === []
 
-prop_getObjectsR :: Token -> Text -> Key -> Key -> Property
-prop_getObjectsR token t p1 p2 = p1 /= p2 ==> ioProperty .
-  runS3WithDefaults . withToken token $ \root -> do
-    let keys = [p1, p2 </> p1, p2 </> p2]
-    forM_ keys $ \k -> write (withKey (</> k) root) t
-    objs <- getObjectsRecursively root
-    pure $ on (===) sort (S3.objectKey <$> objs) (unKey . (</>) (key root) <$> keys)
+prop_getObjectsR :: Text -> Key -> Key -> Property
+prop_getObjectsR t p1 p2 = p1 /= p2 ==> testS3 $ \root -> do
+  let keys = [p1, p2 </> p1, p2 </> p2]
+  forM_ keys $ \k -> write (withKey (</> k) root) t
+  objs <- getObjectsRecursively root
+  pure $ on (===) sort (S3.objectKey <$> objs) (unKey . (</>) (key root) <$> keys)
 
-prop_listRecursively :: Token -> Property
-prop_listRecursively t = monadicIO $
-  stop =<< (run . runS3WithDefaults . withToken t $ \a -> do
-    write a ""
-    r' <- listRecursively (a { key = dirname $ key a })
-    pure $ a `elem` r')
+prop_listRecursively :: Property
+prop_listRecursively = testS3 $ \a -> do
+  write a ""
+  r' <- listRecursively (a { key = dirname $ key a })
+  pure $ a `elem` r'
 
-prop_list :: Token -> Property
-prop_list t = forAll ((,) <$> elements muppets <*> elements southpark) $ \(m, s) -> monadicIO $
-  stop =<< (run . runS3WithDefaults . withToken t $ \a -> do
-    write (withKey(</> Key m) a) ""
-    write (withKey(</> (Key s </> Key m)) a) ""
-    r' <- list a
-    pure $ [m, s <> "/"] === (replace (addressToText a <> "/") "" . addressToText <$> r'))
+prop_list :: Property
+prop_list = forAll ((,) <$> elements muppets <*> elements southpark) $ \(m, s) -> testS3 $ \a -> do
+  write (withKey(</> Key m) a) ""
+  write (withKey(</> (Key s </> Key m)) a) ""
+  r' <- list a
+  pure $ [m, s <> "/"] === (replace (addressToText a <> "/") "" . addressToText <$> r')
 
-prop_getObjs :: Token -> Property
-prop_getObjs t = forAll ((,) <$> elements muppets <*> choose (1000, 1500)) $ \(m, n) -> once . monadicIO $
-  stop =<< (run . runS3WithDefaults . withToken t $ \a -> do
-    forM_ [1..n] $ \n' -> write (withKey(</> Key (m <> pack (show n'))) a) ""
-    r' <- list a
-    pure $ length r' === n)
+prop_getObjs :: Property
+prop_getObjs = forAll ((,) <$> elements muppets <*> choose (1000, 1500)) $ \(m, n) -> once . testS3 $ \a -> do
+  forM_ [1..n] $ \n' -> write (withKey(</> Key (m <> pack (show n'))) a) ""
+  r' <- list a
+  pure $ length r' === n
 
-prop_copy :: Text -> Token -> Token -> Property
-prop_copy t s' d' = testIO $ do
-  runS3WithDefaults . withToken s' $ \s ->
-    withToken d' $ \d -> do
-      write s t
-      copy s d
-      es <- exists s
-      ed <- exists d
-      pure $ (es, ed) === (True, True)
+prop_copy :: Text -> Token -> Property
+prop_copy t d' = testS3 $ \s ->
+  withToken d' $ \d -> do
+    write s t
+    copy s d
+    es <- exists s
+    ed <- exists d
+    pure $ (es, ed) === (True, True)
 
-prop_move :: Text -> Token -> Token -> Property
-prop_move t s' d' = testIO $ do
-  runS3WithDefaults . withToken s' $ \s ->
-    withToken d' $ \d -> do
-      write s t
-      move s d
-      es <- exists s
-      ed <- exists d
-      pure $ (es, ed) === (False, True)
+prop_move :: Text -> Token -> Property
+prop_move t d' = testS3 $ \s ->
+  withToken d' $ \d -> do
+    write s t
+    move s d
+    es <- exists s
+    ed <- exists d
+    pure $ (es, ed) === (False, True)
 
+testLocalS3 :: Testable a => (FilePath -> Address -> S3Action a) -> Property
+testLocalS3 f =
+  property $ \t -> testIO . withSystemTempDirectory "mismi" $ \p ->
+    runS3WithDefaults . withToken t $ \a ->
+      f p a
+
+testS3 :: Testable a => (Address -> S3Action a) -> Property
+testS3 f =
+  property $ \t -> testIO .
+    runS3WithDefaults . withToken t $ \a ->
+      f a
 
 return []
 tests :: IO Bool
