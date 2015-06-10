70345185ab2717d27be7e865583687610e3c4b3c
Nick Hibberd <nhibberd@gmail.com>
2015-06-10 20:17:27 +1000

    Adding retryAction

--- test/Test/Mismi/S3/Control.hs
+++ test/Test/Mismi/S3/Control.hs
@@ -5,6 +5,7 @@
 module Test.Mismi.S3.Control where
 
 import           Control.Monad.IO.Class
+import           Control.Monad.Catch
 
 import           Data.IORef
 import           Data.Text
@@ -12,6 +13,8 @@ import           Data.Text
 import           Disorder.Core.IO
 
 import           Mismi.S3.Control
+
+import           Network.HTTP.Client (HttpException (..))
 
 import           Test.Mismi
 import           Test.Mismi.S3
@@ -36,6 +39,19 @@ prop_liftS3 r t = testAWS r $ do
   liftS3Action (liftIO $ modifyIORef ref (<> t) )
   f <- liftIO $ readIORef ref
   pure $ f === t
+
+prop_retry :: Text -> Property
+prop_retry t = t /= "" ==> testIO $ do
+  ref <- newIORef ""
+  let action = do
+        let m = modifyIORef ref (<> t)
+        z <- readIORef ref
+        if (z == "")
+          then m >> throwM ResponseTimeout
+          else m
+  retryHttp 2 action
+  z <- readIORef ref
+  pure $ z === t <> t
 
 return []
 tests :: IO Bool
