b8662bcfee4a05cbd7f59ccb88c74d7989222abb
Eric Torreborre <etorreborre@yahoo.com>
2015-06-24 17:29:27 +1000

    added a listParts function to list the parts for a given multi-upload

--- mismi-s3/test/Test/IO/Mismi/S3/Amazonka.hs
+++ mismi-s3/test/Test/IO/Mismi/S3/Amazonka.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TemplateHaskell #-}
+{-# OPTIONS_GHC -fno-warn-missing-signatures #-}
 module Test.IO.Mismi.S3.Amazonka where
 
 import           Data.Text (Text)
@@ -14,6 +15,7 @@ import           Mismi.S3.Control
 import           Mismi.S3.Data
 import           Mismi.Control.Amazonka
 
+import           Disorder.Core
 import           Disorder.Core.IO
 
 import           P
@@ -26,29 +28,40 @@ import           Test.QuickCheck
 import           Test.QuickCheck.Instances ()
 
 
-prop_list_multipart :: Property
 prop_list_multipart = withMultipart $ \a i -> do
   sendMultipart "" a 1 i
   l <- listMultiparts (bucket a)
-  pure $ (length $ f' l i) === 1
+  pure $ multipartExists i l
 
-prop_abort_multipart :: Property
 prop_abort_multipart = withMultipart $ \a i -> do
   sendMultipart "" a 1 i
   l <- listMultiparts (bucket a)
-  forM_ (f' l i) $ abortMultipart (bucket a)
+  forM_ (findMultiparts i l) $ abortMultipart (bucket a)
   r <- listMultiparts (bucket a)
-  pure $ (length $ f' r i) === 0
+  pure (neg $ multipartExists i r)
 
-f' :: [MultipartUpload] -> Text -> [MultipartUpload]
-f' l t =
-  filter (\p -> p ^. muUploadId == Just t) l
+prop_list_parts = withMultipart $ \a i -> do
+  sendMultipart "" a 1 i
+  l2 <- listMultipartParts (bucket a) (key a) i
+  pure (length l2 === 1)
+
+multipartExists :: Text -> [MultipartUpload] -> Property
+multipartExists uploadId multiparts = count (findMultipart uploadId) multiparts === 1
+
+findMultiparts :: Text -> [MultipartUpload] -> [MultipartUpload]
+findMultiparts uploadId = filter (findMultipart uploadId)
+
+findMultipart :: Text -> MultipartUpload -> Bool
+findMultipart uploadId m = m ^. muUploadId == Just uploadId
+
+count :: (a -> Bool) -> [a] -> Int
+count predicate list = length (filter predicate list)
 
 withMultipart :: Testable a => (Address -> Text -> AWS a) -> Property
 withMultipart f =
   property $ \t ->
     testIO . runS3WithDefaults . withToken t $ \a ->
-      liftAWSAction $ do
+      liftAWSAction $
         awsBracket (createMultipart a) (abortMultipart' a) (f a)
 
 return []
