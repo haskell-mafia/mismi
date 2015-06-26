a364599dc72986b2f0bae66ae7469c7c441338f3
Michael Neale <michael.neale@gmail.com>
2015-06-26 10:39:05 +1000

    This may save others some insanity, arbs for Security key stuff

--- mismi-core/test/Test/Mismi/Arbitrary.hs
+++ mismi-core/test/Test/Mismi/Arbitrary.hs
@@ -6,8 +6,24 @@ module Test.Mismi.Arbitrary where
 import           Network.AWS.Types
 
 import           Test.QuickCheck
+import           Test.QuickCheck.Instances ()
+
+import           Data.Text
+import           Data.Text.Encoding
+
+import           P
+
 
 
 instance Arbitrary Region where
   -- Shorter list than possible, aws doesn't support all potential Regions.
   arbitrary = elements [Ireland, Tokyo, Singapore, Sydney, NorthCalifornia, Oregon]
+
+instance Arbitrary AccessKey where
+  arbitrary = AccessKey <$> (encodeUtf8 <$> arbitrary)
+
+instance Arbitrary SecretKey where
+  arbitrary = SecretKey <$> (encodeUtf8 <$> arbitrary)
+
+instance Arbitrary SecurityToken where
+  arbitrary = SecurityToken <$> (encodeUtf8 <$> arbitrary)
