888bd4359f085179343112964d113cc38b85b3e5
Nick Hibberd <nhibberd@gmail.com>
2015-10-01 10:20:27 +1000

    Adding mismi-autoscaling

---
+++ mismi-autoscaling/src/Mismi/Autoscaling/Amazonka.hs
@@ -0,0 +1,8 @@
+{-# LANGUAGE NoImplicitPrelude  #-}
+module Mismi.Autoscaling.Amazonka (
+    module AWS
+  ) where
+
+import           Mismi.Amazonka as AWS
+
+import           Network.AWS.AutoScaling as AWS
