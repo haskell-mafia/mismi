db2a81ee4f300a40eab0fc999e18b241df548ad7
Nick Hibberd <nhibberd@gmail.com>
2015-10-05 20:23:48 +1100

    Export amazonka prelude

--- mismi-core/src/Mismi/Amazonka.hs
+++ mismi-core/src/Mismi/Amazonka.hs
@@ -5,8 +5,8 @@ module Mismi.Amazonka (
     module A
   ) where
 
-
 import           Network.AWS as A
 import           Network.AWS.Data as A
 import           Network.AWS.Error as A
 import           Network.AWS.Waiter as A
+import           Network.AWS.Prelude as A
