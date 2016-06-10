1f97fa5e5ce0a6ccb4c10bd026498fbcf511f5e3
mth <mark@hibberd.id.au>
2016-06-10 15:28:29 +1000

    Split out mismi-s3-core.

--- mismi-s3/src/Mismi/S3/Data.hs
+++ mismi-s3/src/Mismi/S3/Data.hs
@@ -41,16 +41,16 @@ module Mismi.S3.Data (
 
 import           Control.Exception.Base
 
-import           Data.Attoparsec.Text hiding (parse, Fail)
-import qualified Data.Attoparsec.Text as AT
 import qualified Data.Text as T
-import           Data.List (init, zipWith)
-import           Data.String
 import           Data.Typeable
 
 import           P
 
 import           Mismi (Error, renderError)
+                 -- Just for compatibility, would be good to not do
+                 -- this at some point but for now we import everything
+                 -- and keep current export list.
+import           Mismi.S3.Core.Data
 import           Network.AWS.S3 (ETag, ServerSideEncryption (..))
 
 import           System.FilePath (FilePath)
@@ -107,11 +107,6 @@ renderErrorType e = case e of
     "download"
   CopyError' ->
     "copy"
-
-data WriteResult =
-    WriteOk
-  | WriteDestinationExists Address
-  deriving (Eq, Show)
 
 data DownloadError =
     DownloadSourceMissing Address
@@ -191,139 +186,10 @@ renderSyncWorkerError w =
       "Copy failure during 'sync': " <> renderCopyError c
 
 
-
-
--- |
--- Describes the semantics for destructive operation that may result in overwritten files.
---
-data WriteMode =
-    Fail        -- ^ Fail rather than overwrite any data.
-  | Overwrite   -- ^ Overwrite existing data silently, i.e. we really want to do this.
-  deriving (Eq, Show)
-
-foldWriteMode :: a -> a -> WriteMode -> a
-foldWriteMode f o = \case
-  Fail -> f
-  Overwrite -> o
-
-data SyncMode =
-  FailSync
-  | OverwriteSync
-  | SkipSync
-  deriving (Eq, Show)
-
-foldSyncMode :: a -> a -> a -> SyncMode -> a
-foldSyncMode f o s = \case
-  FailSync -> f
-  OverwriteSync -> o
-  SkipSync -> s
-
-newtype Bucket = Bucket {
-    unBucket :: Text
-  } deriving (Eq, Show, Ord)
-
-data Address = Address {
-    bucket :: Bucket
-  , key :: Key
-  } deriving (Eq, Ord)
-
--- NOTE: This is not a "safe" data type, and makes no guarantee about what is _actually_ supported for S3
--- https://github.com/ambiata/mismi/issues/2
-newtype Key = Key {
-    unKey :: Text
-  } deriving (Eq, Show, Ord)
-
-newtype ReadGrant =
-  ReadGrant {
-      readGrant :: Text
-    } deriving (Eq, Show)
-
-instance Show Address where
-  show (Address b k) =
-    "Address (" <> show b <> ") (" <> show k <> ")"
-
 data Upload =
     UploadSingle
   | UploadMultipart Integer Integer
   deriving (Eq, Show)
-
-(//) :: Key -> Key -> Key
-(//) =
-  combineKey
-
-combineKey :: Key -> Key -> Key
-combineKey (Key p1) (Key p2) =
-  if  "/" `T.isSuffixOf` p1 || p1 == "" || "/" `T.isPrefixOf` p2
-    then Key $ p1 <> p2
-    else Key $ p1 <> "/" <> p2
-
-withKey :: (Key -> Key) -> Address -> Address
-withKey f (Address b k) =
-  Address b $ f k
-
-
-dirname :: Key -> Key
-dirname =
-  Key . T.intercalate "/" . init . T.split (=='/') . unKey
-
--- | Get the basename for a given key (eg. basename "/foo/bar" == "bar").
---   Return 'Nothing' for the empty 'Key' _and_ when the name ends with a '/'.
-basename :: Key -> Maybe Text
-basename =
-  mfilter (not . T.null) . listToMaybe . reverse . T.split (== '/') . unKey
-
--- prefix key
-removeCommonPrefix :: Address -> Address -> Maybe Key
-removeCommonPrefix prefix addr =
-  let dropMaybe :: String -> String -> Maybe Text
-      dropMaybe x y =
-        bool
-          Nothing
-          (Just . T.pack $ drop (length y) x)
-          (check x y)
-      check :: String -> String -> Bool
-      check x y = y == zipWith const x y
-  in
-  if bucket addr == bucket prefix
-     then
-       if unKey (key prefix) == ""
-          then
-            Just $ key addr
-          else
-            let bk = unKey (key prefix)
-                b = bool (bk <> "/") bk ("/" `T.isSuffixOf` bk)
-                pk = T.unpack b
-                kk = T.unpack (unKey $ key addr)
-            in
-              Key <$> dropMaybe kk pk
-     else
-       Nothing
-
-addressToText :: Address -> Text
-addressToText a =
-  "s3://" <> unBucket (bucket a) <> "/" <> unKey (key a)
-
-addressFromText :: Text -> Maybe Address
-addressFromText =
-  rightToMaybe . AT.parseOnly s3Parser
-
-s3Parser :: Parser Address
-s3Parser =
-  s3Parser' <|> s3Parser''
-
-s3Parser' :: Parser Address
-s3Parser' = do
-  _ <- string "s3://"
-  b <- manyTill anyChar (char '/')
-  k <- many anyChar
-  pure $ Address (Bucket . T.pack $ b) (Key . T.pack $ k)
-
-s3Parser'' :: Parser Address
-s3Parser'' = do
-  _ <- string "s3://"
-  b <- takeWhile (/= '/')
-  endOfInput
-  pure $ Address (Bucket b) (Key "")
 
 sse :: ServerSideEncryption
 sse =
--- mismi-s3/test/test.hs
+++ mismi-s3/test/test.hs
@@ -2,7 +2,6 @@ import           Disorder.Core.Main
 
 import qualified Test.Mismi.S3.Commands
 import qualified Test.Mismi.S3.Control
-import qualified Test.Mismi.S3.Data
 import qualified Test.Mismi.S3.Internal
 
 
@@ -11,6 +10,5 @@ main =
   disorderMain [
       Test.Mismi.S3.Commands.tests
     , Test.Mismi.S3.Control.tests
-    , Test.Mismi.S3.Data.tests
     , Test.Mismi.S3.Internal.tests
     ]
