d4d2084c953f8130a415195bce376c99209f7bc0
Nick Hibberd <nhibberd@gmail.com>
2016-06-13 20:45:26 +1000

    Adding mismi-openssl

---
+++ mismi-openssl/src/Mismi/OpenSSL/Environment.hs
@@ -0,0 +1,61 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+module Mismi.OpenSSL.Environment (
+    X.Env
+  , X.Region (..)
+  , X.RegionError (..)
+  , X.Debugging (..)
+  , X.renderRegionError
+  , X.getRegionFromEnv
+  , X.getDebugging
+  , X.setDebugging
+  , X.withOpenSSL
+  , discoverAWSEnv
+  , discoverAWSEnvWithRegion
+  , discoverAWSEnvRetry
+  , discoverAWSEnvWithRegionRetry
+  ) where
+
+import           Control.Lens ((.~))
+import           Control.Monad.Catch (Handler(..))
+import           Control.Monad.Trans.AWS (Credentials(..), Region(..), Env)
+import           Control.Monad.Trans.AWS (newEnv)
+import qualified Control.Monad.Trans.AWS as X
+import           Control.Monad.Trans.Class (lift)
+import           Control.Retry (RetryPolicyM, recovering, constantDelay, limitRetries)
+
+import           Network.AWS.Env (envManager)
+import           Network.HTTP.Client (newManager)
+import           Network.HTTP.Client.OpenSSL (opensslManagerSettings)
+import qualified Network.HTTP.Client.OpenSSL as X
+import           OpenSSL.Session (context)
+
+import           Mismi.Environment (RegionError (..))
+import           Mismi.Environment (getDebugging, setDebugging, catchAuthError)
+import qualified Mismi.Environment as X
+
+import           P
+
+import           System.IO (IO)
+
+import           X.Control.Monad.Trans.Either (EitherT)
+
+discoverAWSEnv :: EitherT RegionError IO Env
+discoverAWSEnv =
+  discoverAWSEnvRetry $ limitRetries 1 <> constantDelay 200000
+
+discoverAWSEnvWithRegion :: Region -> IO Env
+discoverAWSEnvWithRegion r =
+  flip discoverAWSEnvWithRegionRetry r $ limitRetries 1 <> constantDelay 200000
+
+discoverAWSEnvRetry :: RetryPolicyM IO -> EitherT RegionError IO Env
+discoverAWSEnvRetry retry = do
+  r <- X.getRegionFromEnv
+  lift $ discoverAWSEnvWithRegionRetry retry r
+
+discoverAWSEnvWithRegionRetry :: RetryPolicyM IO -> Region -> IO Env
+discoverAWSEnvWithRegionRetry rpol r = do
+  d <- getDebugging
+  e <- recovering rpol [(\_ -> Handler catchAuthError)] $ \_ -> newEnv r Discover
+  m <- newManager $ opensslManagerSettings context
+  pure $ setDebugging d (e & envManager .~ m)
