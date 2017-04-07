467ecf32f5e0ded0c3d3d605e007fd25dab0f455
Erik de Castro Lopo <erik.decastrolopo@ambiata.com>
2017-04-07 14:28:02 +1000

    kernel: New regions in amazonka

--- mismi-kernel/src/Mismi/Kernel/Data.hs
+++ mismi-kernel/src/Mismi/Kernel/Data.hs
@@ -14,16 +14,20 @@ data MismiRegion =
     IrelandRegion         -- ^ Europe / eu-west-1
   | FrankfurtRegion       -- ^ Europe / eu-central-1
   | TokyoRegion           -- ^ Asia Pacific / ap-northeast-1
+  | SeoulRegion           -- ^ Asia Pacific / ap-northeast-2
   | SingaporeRegion       -- ^ Asia Pacific / ap-southeast-1
   | SydneyRegion          -- ^ Asia Pacific / ap-southeast-2
+  | MumbaiRegion          -- ^ Asia Pacific / ap-south-1
   | BeijingRegion         -- ^ China / cn-north-1
   | NorthVirginiaRegion   -- ^ US / us-east-1
+  | OhioRegion            -- ^ US / us-east-2
   | NorthCaliforniaRegion -- ^ US / us-west-1
   | OregonRegion          -- ^ US / us-west-2
   | GovCloudRegion        -- ^ AWS GovCloud / us-gov-west-1
   | GovCloudFIPSRegion    -- ^ AWS GovCloud (FIPS 140-2) S3 Only / fips-us-gov-west-1
   | SaoPauloRegion        -- ^ South America / sa-east-1
     deriving (Eq, Ord, Read, Show, Enum, Bounded)
+
 
 
 renderMismiRegion :: MismiRegion -> Text
@@ -35,14 +39,20 @@ renderMismiRegion r =
       "eu-central-1"
     TokyoRegion ->
       "ap-northeast-1"
+    SeoulRegion ->
+      "ap-northeast-2"
     SingaporeRegion ->
       "ap-southeast-1"
     SydneyRegion ->
       "ap-southeast-2"
+    MumbaiRegion ->
+      "ap-south-1"
     BeijingRegion ->
       "cn-north-1"
     NorthVirginiaRegion ->
       "us-east-1"
+    OhioRegion ->
+      "us-east-2"
     NorthCaliforniaRegion ->
       "us-west-1"
     OregonRegion ->
@@ -63,14 +73,20 @@ parseMismiRegion t =
       Just FrankfurtRegion
     "ap-northeast-1" ->
       Just TokyoRegion
+    "ap-northeast-2" ->
+      Just SeoulRegion
     "ap-southeast-1" ->
       Just SingaporeRegion
     "ap-southeast-2" ->
       Just SydneyRegion
+    "ap-south-1" ->
+      Just MumbaiRegion
     "cn-north-1" ->
       Just BeijingRegion
     "us-east-1" ->
       Just NorthVirginiaRegion
+    "us-east-2" ->
+      Just OhioRegion
     "us-west-2" ->
       Just OregonRegion
     "us-west-1" ->
